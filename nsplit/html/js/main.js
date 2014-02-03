5f732171296887d69dc0f9809896d0172258e54d
gmamaladze <gmamaladze@gmail.com>
2014-02-03 23:03:27 +0100

    Added pan and zoom functionality

--- nsplit/html/js/main.js
+++ nsplit/html/js/main.js
@@ -242,3 +242,16 @@ function resize() {
         .size([w, h])
         .resume();
 }
+
+$(document).ready(function() {
+    var panzoom = $("#viewport svg").panzoom();
+    panzoom.parent().on('mousewheel.focal', function(e) {
+        e.preventDefault();
+        var delta = e.delta || e.originalEvent.wheelDelta;
+        var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+        panzoom.panzoom('zoom', zoomOut, {
+            increment: 0.1,
+            focal: e
+        });
+    });
+});
