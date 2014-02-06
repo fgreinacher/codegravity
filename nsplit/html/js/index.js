1ff4e6b25ac0e8190d04005a6d577a3dae77ebe5
gmamaladze <gmamaladze@gmail.com>
2014-02-06 08:15:36 +0100

    Extracted index.js

---
+++ nsplit/html/js/index.js
@@ -0,0 +1,39 @@
+﻿// This code is distributed under MIT license. 
+// Copyright (c) 2014 George Mamaladze, Florian Greinacher
+// See license.txt or http://opensource.org/licenses/mit-license.php
+function startAnalyzes(file) {
+    $.getJSON("api/analyzes/start?file=" + file, function(data) {
+        $("#loaded").text(data.message);
+        $("#message").text("Started");
+        if (data.ok) {
+            setTimeout(getPorgress, 100);
+            $("#goto")
+                .removeAttr("disabled")
+                .on("click", goToGraph);
+        }
+    });
+}
+
+function getPorgress() {
+    $.getJSON("api/analyzes/progress", function(progress) {
+        $("#progress").text(Math.round(progress.actual * 100 / progress.max) + "%");
+        $("#message").text(progress.message);
+        if (progress.finished) goToGraph();
+        else setTimeout(getPorgress, 200);
+    });
+}
+
+function goToGraph() {
+    window.location.href = "main.html";
+}
+
+$(document).ready(function () {
+    $("#analyze").on("click", function () {
+        var fileName = $("#assembly").val();
+        startAnalyzes(fileName);
+    });
+
+    $("#demo").on("click", function () {
+        startAnalyzes("");
+    });
+});
